b62c3873395968f5ea79c2e5b3bb393c2a43a9fb
srescio <simone.rescio@gmail.com>
2017-10-07 15:51:28 +0200

    ISSUE-2 rifattorizzato la parte HTML per migliorarne lettura e compresione tramite l'indentazione dei tag, introdotto ciclo for per stampare i pulsanti presenti in configurazione

--- src/js/agid-spid-enter.js
+++ src/js/agid-spid-enter.js
@@ -1,57 +1,85 @@
-function insertCode() {
-var agid_spid_enter = document.getElementById('agid-spid-enter');
-        var panel_html = '\
-		<div id="agid-spid-button-anim">\
-			<div id="agid-spid-button-anim-base"></div>\
-		    <div id="agid-spid-button-anim-icon"></div>\
-		</div>	\
-		<section id="agid-spid-panel-select" class="agid-spid-panel" aria-labelledby="agid-spid-enter-title-page" hidden>\
-			<header id="agid-spid-panel-header">\
-                        <nav class="agid-spid-panel-back agid-spid-panel-element" aria-controls="agid-spid-panel-select">\
-				<div role="button">	<a href="#" onclick="hidePanel(\'agid-spid-panel-select\')" class="agid-spid-button" >\
-						<img src="img/x-icon.svg" alt="Torna indietro">\
-					</a>\
-				</div></nav>\
-				<div class="agid-spid-panel-logo agid-spid-panel-element">\
-					<img aria-hidden="true" src="img/spid-logo-c-lb.svg" alt="Logo SPID">\
-				</div>\
-                                			</header>\
-			<div id="agid-spid-panel-content">\
-				<div class="agid-spid-panel-content-center">\
-                                <div id ="agid-spid-enter-title-container" ><h1 id="agid-spid-enter-title-page">Scegli il tuo provider SPID</h1>\
-                                </div> \
-					<div id="agid-spid-idp-list" class="agid-spid-row">'
+function displaySPIDConfiguredProviders() {
+    var agid_spid_enter = document.getElementById('agid-spid-enter'),
+        panel_html      = [
+            '<div id="agid-spid-button-anim">',
+                '<div id="agid-spid-button-anim-base"></div>',
+                '<div id="agid-spid-button-anim-icon"></div>',
+            '</div>',
+            '<section id="agid-spid-panel-select" class="agid-spid-panel" aria-labelledby="agid-spid-enter-title-page" hidden>',
+                '<header id="agid-spid-panel-header">',
+                    '<nav class="agid-spid-panel-back agid-spid-panel-element" aria-controls="agid-spid-panel-select">',
+                        '<div role="button">',
+                            '<a href="#" onclick="hidePanel(\'agid-spid-panel-select\')" class="agid-spid-button">',
+                                '<img src="img/x-icon.svg" alt="Torna indietro">',
+                            '</a>',
+                        '</div>',
+                    '</nav>',
+                    '<div class="agid-spid-panel-logo agid-spid-panel-element">',
+                        '<img aria-hidden="true" src="img/spid-logo-c-lb.svg" alt="Logo SPID">',
+                    '</div>',
+                '</header>',
+                '<div id="agid-spid-panel-content">',
+                    '<div class="agid-spid-panel-content-center">',
+                        '<div id ="agid-spid-enter-title-container">',
+                            '<h1 id="agid-spid-enter-title-page">Scegli il tuo provider SPID</h1>',
+                        '</div>',
+                        '<div id="agid-spid-idp-list" class="agid-spid-row">'
+        ].join('');
 
-        if (config["infocert"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["infocert"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-infocertid" title="' + config["infocert"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["infocert"]["logo"] + '\')"></a></span>';
-        if (config["poste"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["poste"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-posteid" title="' + config["poste"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["poste"]["logo"] + '\')"></a></span>';
-        if (config["tim"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["tim"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-timid" title="' + config["tim"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["tim"]["logo"] + '\')"></a></span>';
-        if (config["sielte"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["sielte"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-sielteid" title="' + config["sielte"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["sielte"]["logo"] + '\')"></a></span>';
-        if (config["aruba"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["aruba"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-arubaid" title="' + config["aruba"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["aruba"]["logo"] + '\')"></a></span>';
-        if (config["newidp1"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["newidp1"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-dummy" title="' + config["newidp1"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["newidp1"]["logo"] + '\')"></a></span>';
-        if (config["newidp2"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["newidp2"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-dummy" title="' + config["newidp2"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["newidp2"]["logo"] + '\')"></a></span>';
-        if (config["newidp3"]["url"]) panel_html += '<span class="agid-spid-col l3 m6 s6 xs12" role="button"><a href="' + config["newidp3"]["url"] + '" class="agid-spid-idp-button agid-spid-idp-dummy" title="' + config["newidp3"]["title"] + '" style="background-image: url(\'img/idp-logos/' + config["newidp3"]["logo"] + '\')"></a></span>';
-        panel_html += '\
-					</div>\
-				<div id="agid-cancel-access-container">\
-					<a href="#"  onclick="hidePanel(\'agid-spid-panel-select\')">   <div id="agid-cancel-access-button" class="agid-transparent-button" role="button"><span>Annulla l\'accesso</span></div></a>\
-				</div > \
-                                <div id="agid-logo-container" aria-hidden="true">\
-					<img id="agid-logo" class="agid-logo" src="./img/agid-logo-bb-short.png" aria-hidden="true"/>\
-				</div > \
-			</div>\
-                        </div>\
-			<footer id="agid-spid-panel-footer">\
-                            <div id="agid-action-button-container">\
-                                <a href="#"> <div id="nospid" class="agid-action-button" role="button"><span>Non hai SPID?</span></div></a>\
-                                <a href="#"> <div id="cosaspid" class="agid-action-button" role="button"><span>Cos\'e SPID?</span></div></a>\
-                            </div>\
-                        </footer>\
-		</section>';
-        agid_spid_enter.innerHTML = panel_html;
-        
-        document.getElementById("nospid").addEventListener('click', function(ev) { openmodal('Non hai SPID?'); });
-        document.getElementById("cosaspid").addEventListener('click', function(ev) { openmodal('Cos\'è SPID?');});
-        }
+    // Cicla l'oggetto config e aggiunge i pulsanti dei provider disponibili
+    for (var provider in config) {
+        var providerData = config[provider];
+
+        if (!providerData.url) return;
+
+        panel_html += [
+            '<span class="agid-spid-col l3 m6 s6 xs12" role="button">',
+                '<a href="', providerData.url, '"',
+                    // la seconda classe è necessaria? non è in nessun CSS, potrebbe essere messa in config, oppure affidarsi al nome della config
+                    'class="agid-spid-idp-button agid-spid-idp-infocertid"',
+                    'title="', providerData.title, '"',
+                    'style="background-image: url(\'img/idp-logos/' + providerData.logo, '\')">',
+                '</a>',
+            '</span>'
+        ].join('');
+    };
+
+    panel_html += [
+                    '</div>',
+                    '<div id="agid-cancel-access-container">',
+                        '<a href="#"  onclick="hidePanel(\'agid-spid-panel-select\')">',
+                            '<div id="agid-cancel-access-button" class="agid-transparent-button" role="button">',
+                                '<span>Annulla l\'accesso</span>',
+                            '</div>',
+                        '</a>',
+                    '</div>',
+                    '<div id="agid-logo-container" aria-hidden="true">',
+                        '<img id="agid-logo" class="agid-logo" src="./img/agid-logo-bb-short.png" aria-hidden="true" />',
+                    '</div>',
+                '</div>',
+            '</div>',
+            '<footer id="agid-spid-panel-footer">',
+                '<div id="agid-action-button-container">',
+                    '<a href="#">',
+                        '<div id="nospid" class="agid-action-button" role="button">',
+                            '<span>Non hai SPID?</span>',
+                        '</div>',
+                    '</a>',
+                    '<a href="#">',
+                        '<div id="cosaspid" class="agid-action-button" role="button">',
+                            '<span>Cos\'e SPID?</span>',
+                        '</div>',
+                    '</a>',
+                '</div>',
+            '</footer>',
+        '</section>'
+    ].join('');
+
+    agid_spid_enter.innerHTML = panel_html;
+
+    document.getElementById("nospid").addEventListener('click', function(ev) { openmodal('Non hai SPID?'); });
+    document.getElementById("cosaspid").addEventListener('click', function(ev) { openmodal('Cos\'è SPID?');});
+}
 
 function insertButtonS() {
 var agid_spid_enter_button_xl = document.getElementsByClassName("agid-spid-enter-button-size-s");
@@ -204,7 +232,8 @@ ul.appendChild(ul.children[Math.random() * i | 0]);
 }
 
 function agid_spid_enter() {
-insertCode();
+        displaySPIDConfiguredProviders();
+
         insertButtonS();
         insertButtonM();
         insertButtonL();
