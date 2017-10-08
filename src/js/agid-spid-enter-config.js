72e99be13a30d85bbbb52357412d60d76b4169fe
srescio <simone.rescio@gmail.com>
2017-10-08 15:07:00 +0200

    ISSUE-2 creato fake endpoint pubblico su mocky.io con access header all per far funzionare la demo anche aprendo l'index senza local server netbeans

--- src/js/agid-spid-enter-config.js
+++ src/js/agid-spid-enter-config.js
@@ -4,5 +4,5 @@
  */
 
 window.agidSpidEnterConfig = {
-    spidProvidersEndpoint: '/spid-smart-button/src/data/spidProviders-success.json'
+    spidProvidersEndpoint: 'http://www.mocky.io/v2/59da22a3100000a411caf1c3'
 };
