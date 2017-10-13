7f69cbb3eee36782f3622e2d0724d355773a9705
srescio <simone.rescio@gmail.com>
2017-10-14 01:01:32 +0200

    ISSUE-2 ripristinato corretto check indent con livello error

--- src/js/agid-spid-enter-tpl.js
+++ src/js/agid-spid-enter-tpl.js
@@ -102,10 +102,10 @@ window.agidSpidEnterTpl = {
     nonHaiSpid: function () {
         return [
             '<ul>',
-                '<li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>',
-                '<li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>',
-                '<li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>',
-                '<li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>',
+            '<li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>',
+            '<li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>',
+            '<li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>',
+            '<li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>',
             '</ul>'
         ].join('');
     },
@@ -116,14 +116,14 @@ window.agidSpidEnterTpl = {
             '<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>',
             '<h2>Header Level 2</h2>',
             '<ol>',
-			   '<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>',
-			   '<li>Aliquam tincidunt mauris eu risus.</li>',
+			    '<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>',
+			    '<li>Aliquam tincidunt mauris eu risus.</li>',
             '</ol>',
             '<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>',
             '<h3>Header Level 3</h3>',
             '<ul>',
-			   '<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>',
-			   '<li>Aliquam tincidunt mauris eu risus.</li>',
+			    '<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>',
+			    '<li>Aliquam tincidunt mauris eu risus.</li>',
             '</ul>'
 	    ].join('');
     }
