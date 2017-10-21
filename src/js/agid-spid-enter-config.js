7ed5b80502a1e036df8a3ef96b7f58352dfc895d
srescio <simone.rescio@gmail.com>
2017-10-21 20:49:21 +0200

    ISSUE-2 aggiornate url config temporanee di prod con puntamenti a rawgit

--- src/js/agid-spid-enter-config.js
+++ src/js/agid-spid-enter-config.js
@@ -20,10 +20,10 @@ unless you know what you're doing... :)
  * Indirizzi di produzione delle risorse (attualmente test), inserire indirizzi reali
  */
 window.AgidSpidEnter.prototype.config = {
-    assetsBaseUrl: 'https://raw.githubusercontent.com/srescio/spid-smart-button/issue-2/separate-html-js/',
-    stylesheetUrl: 'https://raw.githubusercontent.com/srescio/spid-smart-button/issue-2/separate-html-js/prod/agid-spid-enter.min.css',
-    providersEndpoint: 'https://raw.githubusercontent.com/srescio/spid-smart-button/issue-2/separate-html-js/src/data/spidProviders.json',
-    localisationEndpoint: 'https://raw.githubusercontent.com/srescio/spid-smart-button/issue-2/separate-html-js/src/data/spidI18n.json'
+    assetsBaseUrl: 'https://rawgit.com/srescio/spid-smart-button/issue-2/separate-html-js/',
+    stylesheetUrl: 'https://rawgit.com/srescio/spid-smart-button/issue-2/separate-html-js/prod/agid-spid-enter.min.css',
+    providersEndpoint: 'https://rawgit.com/srescio/spid-smart-button/issue-2/separate-html-js/src/data/spidProviders.json',
+    localisationEndpoint: 'https://rawgit.com/srescio/spid-smart-button/issue-2/separate-html-js/src/data/spidI18n.json'
 };
 
 /*
