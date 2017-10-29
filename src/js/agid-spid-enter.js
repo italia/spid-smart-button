e6c85b979a361f6b745d95db980275bb5e8f111d
srescio <simone.rescio@gmail.com>
2017-10-29 20:16:18 +0100

    ISSUE-2 aggiornato timeout

--- src/js/agid-spid-enter.js
+++ src/js/agid-spid-enter.js
@@ -31,7 +31,7 @@ window.AgidSpidEnter = function () {
     function giveFocusTo(element) {
         setTimeout(function () {
             element.focus();
-        }, 800);
+        }, 900);
     }
 
     function closeInfoModal() {
