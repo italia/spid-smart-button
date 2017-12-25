9a1c0568a1e4472ff8c86d020934764a1f5658b6
srescio <simone.rescio@gmail.com>
2017-12-25 18:52:48 +0100

    ISSUE-2 aggiornati author e contributors, aggiunto check sulla versione node prima dell'install

---
+++ check-node-version.js
@@ -0,0 +1,21 @@
+var packageJson          = require('./package.json'),
+    supportedNodeVersion = packageJson.engines.node,
+    currentNodeVersion   = process.version;
+
+function stripNonDigits(string) {
+    return parseInt(string.replace(/\D/g, ''), 10);
+}
+
+if (stripNonDigits(supportedNodeVersion) !== stripNonDigits(currentNodeVersion)) {
+    console.log('This project was developed with node.js version', supportedNodeVersion);
+}
+
+if (stripNonDigits(supportedNodeVersion) < stripNonDigits(currentNodeVersion)) {
+    console.log('current version detected is ahead:', currentNodeVersion);
+    console.log('If anything is wrong with depencies install try downgranding your node.js version\n\n');
+}
+
+if (stripNonDigits(supportedNodeVersion) > stripNonDigits(currentNodeVersion)) {
+    console.log('current version detected is behind:', currentNodeVersion);
+    console.log('If anything is wrong with depencies install try upgrading your node.js version\n\n');
+}
