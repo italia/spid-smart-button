45277340df4ba15bf511c5fbd951ce18400779be
Alessandro Ranellucci <alessandro@pintle.it>
2018-08-17 18:36:11 +0200

    Use the gh-pages URL until we have a CDN

--- src/js/agid-spid-enter-config.js
+++ src/js/agid-spid-enter-config.js
@@ -5,8 +5,8 @@ var _SPID;
 (function (_SPID) {
     _SPID.prototype.initResources = function () {
         this.resources = {
-            assetsBaseUrl: '',
-            stylesheetUrl: 'dist/spid-button.min.css'
+            assetsBaseUrl: 'https://italia.github.io/spid-smart-button/',
+            stylesheetUrl: 'https://italia.github.io/spid-smart-button/spid-button.min.css'
         };
     };
 })(_SPID || {});
